6dbe8ba790ba78ef2127e29b8b5028bbd98a5be3
leilatrend <leila_kao@trendmicro.com>
2025-07-15 07:50:15 +0000

    fix: Add null safety checks to prevent Save button crash

--- server/FormHandler.js
+++ server/FormHandler.js
@@ -1,5 +1,10 @@
 function handleSave(data) {
-  console.log(data.field.length); // 🐛 potential null pointer
+  // Add null safety checks to prevent crashes
+  if (data && data.field && typeof data.field.length !== 'undefined') {
+    console.log(data.field.length);
+  } else {
+    console.log('No valid field data provided');
+  }
 }
 
 module.exports = { handleSave };
