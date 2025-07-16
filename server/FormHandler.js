6c6f356b0723f1794b1a68d0bfba1e8cb2945ce8
leilatrend <leila_kao@trendmicro.com>
2025-07-16 06:50:41 +0000

    fix: add null check to prevent crash when saving form

--- server/FormHandler.js
+++ server/FormHandler.js
@@ -1,5 +1,11 @@
 function handleSave(data) {
-  console.log(data.field.length); // 🐛 potential null pointer
+  if (data && Array.isArray(data.field)) {
+    console.log(data.field.length);
+  } else {
+    console.warn("Warning: data.field is missing or not an array", data.field);
+    console.log(0);
+  }
 }
 
 module.exports = { handleSave };
+
